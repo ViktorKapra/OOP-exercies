465d923831db087127ec00f0ba39ee19f0dc8c8e
viktorkapra <viktorkapra@gmail.com>
2022-03-04 21:45:38 +0200

    Changing type of luxury class (from int -> double)

--- SeminarTasks_1_03/Tasks/Tasks/Tasks/Task1.cpp
+++ SeminarTasks_1_03/Tasks/Tasks/Tasks/Task1.cpp
@@ -5,69 +5,69 @@ struct ElectricalSupply
 	double kWh;
 	double priceOfProduct;
 	double priceOfInstalation;
-	int luxuryClass;
+	double luxuryClass;
 	ElectricalSupply(double _kWh, double _priceOfProduct,
-		double _priceOfInstalation, int _luxuryClass)
+		double _priceOfInstalation, double _luxuryClass)
 	{
 		kWh = _kWh;
 		priceOfProduct = _priceOfProduct;
 		priceOfInstalation = _priceOfInstalation;
 		luxuryClass = _luxuryClass;
 	}
-	double getPriceOfProduct() { return priceOfProduct; }
+	double getFullPrice()const { return priceOfProduct+ priceOfInstalation; }
 };
-double getKWh(ElectricalSupply e, float priceKWh)
+double getKWh(ElectricalSupply &e, float priceKWh)
 {
 	return e.kWh * priceKWh;
 }
-int getLuxuryClass(ElectricalSupply e)
+int getLuxuryClass(ElectricalSupply &e)
 {
 	return e.luxuryClass;
 }
 struct Microwave :ElectricalSupply
 {
 	Microwave(double _kWh, double _priceOfProduct,
-		double _priceOfInstalation, int _luxuryClass)
+		double _priceOfInstalation, double _luxuryClass)
 	:ElectricalSupply( _kWh, _priceOfProduct, _priceOfInstalation, _luxuryClass)
 	{ }
 };
-double getMicrowavePrice(Microwave m) { return m.getPriceOfProduct(); }
+double getMicrowavePrice(const Microwave &m) { return m.getFullPrice(); }
 
 struct Fridge : ElectricalSupply
 {
 	Fridge(double _kWh, double _priceOfProduct,
-		double _priceOfInstalation, int _luxuryClass)
+		double _priceOfInstalation, double _luxuryClass)
 	:ElectricalSupply(_kWh, _priceOfProduct, _priceOfInstalation, _luxuryClass)
 	{ }
 };
-double getFridgePrice(Fridge m) { return m.getPriceOfProduct(); }
+double getFridgePrice(Fridge m) { return m.getFullPrice(); }
 
 struct Oven : ElectricalSupply
 {
 	Oven(double _kWh, double _priceOfProduct,
-		double _priceOfInstalation, int _luxuryClass)
+		double _priceOfInstalation, double _luxuryClass)
 	:ElectricalSupply(_kWh, _priceOfProduct, _priceOfInstalation, _luxuryClass)
 	{ }
 };
-double getOvenPrice(Oven m) { return m.getPriceOfProduct(); }
+double getOvenPrice(Oven m) { return m.getFullPrice(); }
 
 struct Aspirator : ElectricalSupply
 {
 	Aspirator(double _kWh, double _priceOfProduct,
-		double _priceOfInstalation, int _luxuryClass)
+		double _priceOfInstalation, double _luxuryClass)
 	:ElectricalSupply(_kWh, _priceOfProduct, _priceOfInstalation, _luxuryClass)
 	{ }
 };
-double getAspiratorPrice(Aspirator m) { return m.getPriceOfProduct(); }
+double getAspiratorPrice(Aspirator m) { return m.getFullPrice(); }
 
 struct FoodProcessor : ElectricalSupply
 {
 	FoodProcessor(double _kWh, double _priceOfProduct,
-		double _priceOfInstalation, int _luxuryClass)
+		double _priceOfInstalation, double _luxuryClass)
 	:ElectricalSupply(_kWh, _priceOfProduct, _priceOfInstalation, _luxuryClass)
 	{ }
 };
-double getFoodProcessorPrice(FoodProcessor m) { return m.getPriceOfProduct(); }
+double getFoodProcessorPrice(FoodProcessor m) { return m.getFullPrice(); }
 
 int main()
 {
@@ -78,8 +78,8 @@ int main()
 
 	std::cout << "Total price per hour : "
 	<< getKWh(microwave, POWER_PRICE_KWH) + getKWh(fridge, POWER_PRICE_KWH) << std::endl;
-	std::cout << "Microwave luxury class :" << getLuxuryClass(microwave) << std::endl;
+	std::cout << "Microwave luxury class : " << getLuxuryClass(microwave) << std::endl;
 	std::cout << "Fridge luxury class : " << getLuxuryClass(fridge) << std::endl;
-	std::cout << "Microwave price(" << (INCLUDES_INSTALLATION ? "including" : "excluding") << " installation) :" << getMicrowavePrice(microwave);
+	std::cout << "Microwave price(" << (INCLUDES_INSTALLATION ? "including" : "excluding") << " installation) :" << getMicrowavePrice(microwave)<<std::endl;
 	std::cout << "Fridge price(" << (INCLUDES_INSTALLATION ? "including" : "excluding") << " installation) :" << getFridgePrice(fridge);
 }
